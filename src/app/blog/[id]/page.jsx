2b964132935ea463d3e3287f9b1d59acb97b03fa
Christever <christian@steverlynck.fr>
2024-08-25 18:52:09 +0200

    Mise en page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -8,8 +8,9 @@ export default async function Article({ params: { id } }) {
     const article = await response.json();
 
     return (
-        <>
+        <div className="w-[800px] mx-auto">
             <h2 className="text-center text-3xl">{article.title}</h2>
-        </>
+            <p>{article.content}</p>
+        </div>
     );
 }
